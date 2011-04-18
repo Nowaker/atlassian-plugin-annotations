6018bf9244eb383570d761e165964c00a4f9bcff
Damian Nowak <nowaker@nwkr-sp.(none)>
2011-04-18 12:07:52 +0200

    JIRA Project Tab Panel added

--- src/test/java/net/nowaker/atlassian/annotations/jira/AtlasJiraServletTest.java
+++ src/test/java/net/nowaker/atlassian/annotations/jira/AtlasJiraServletTest.java
@@ -1,9 +1,21 @@
 package net.nowaker.atlassian.annotations.jira;
 
+import org.junit.Test;
+
 import javax.servlet.http.HttpServlet;
 
 @AtlasJiraServlet(
 	urlPattern = "/jira/test"
 )
 public class AtlasJiraServletTest extends HttpServlet {
+
+	@Test
+	public void should() throws Exception {
+		// given
+		
+		// when
+
+		// then
+	}
+	
 }
